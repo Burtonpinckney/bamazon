1753817736d75c0c2cdbb92eaf4d27ec353f10d5
Burton Pinckney <mbpinckney@gmail.com>
2019-08-06 15:39:36 -0400

    Playing with the JS file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,19 +3,19 @@ var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
-  
+
     port: 3306,
-  
+
     user: "root",
-  
+
     password: "root",
     database: "bamazon"
-  });
-  
-  connection.connect(function(err) {
+});
+
+connection.connect(function (err) {
     if (err) throw err;
-   selectProducts();
-  });
+    selectProducts();
+});
 
 function selectProducts() {
     connection.query("SELECT * FROM products", function (err, res) {
@@ -25,10 +25,9 @@ function selectProducts() {
             console.log("ID#- " + res[i].item_id + " | " + res[i].product_name + " | Price: $" + res[i].price + ".00");
         }
         console.log("---------------------------------------------");
+        pickItem();
     });
 }
-
-pickItem();
 
 function pickItem() {
     inquirer
@@ -37,15 +36,34 @@ function pickItem() {
                 name: "item",
                 type: "input",
                 message: "What is the ID# of the item you want to buy?"
-              },
-              {
+            },
+            {
                 name: "units",
                 type: "input",
                 message: "How many units did you want to buy?"
-              }
+            }
         ])
-        .then(function(answer)  {
-            if (answer.units > stock_quanity)
-            console.log("Insufficient quantity!")
-        })
+        .then(function (answer) {
+            var newAnswer = parseInt(answer.item);
+            connection.query("SELECT * FROM products WHERE ?",
+            {
+                item_id: newAnswer
+            },
+            
+            function (err, res) {
+                if (err) throw err;
+                console.log(res)
+                var newQuanity = res.stock_quanity - parseInt(answer.units)
+                    if (answer.units > res.stock_quanity) {
+                     console.log("Insufficient quantity!")
+                    } else {
+                    connection.query("UPDATE products SET ? WHERE ?",
+                        [{stock_quanity: newQuanity}, {item_id: newAnswer}]
+    
+                        )
+                        console.log(newQuanity);
+                    }
+                })
+            })
+            
 }
