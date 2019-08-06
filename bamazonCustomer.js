d1f04ae54dce6e71639b18edc1a0b21a550aacb4
Burton Pinckney <mbpinckney@gmail.com>
2019-08-06 09:58:27 -0400

    SQL should be completed. First of JS completed. Need answer manipulation completed.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,11 +9,43 @@ var connection = mysql.createConnection({
     user: "root",
   
     password: "root",
-    database: "greatBay_DB"
+    database: "bamazon"
   });
   
   connection.connect(function(err) {
     if (err) throw err;
-    
-    start();
+   selectProducts();
   });
+
+function selectProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err)
+            throw err;
+        for (let i = 0; i < res.length; i++) {
+            console.log("ID#- " + res[i].item_id + " | " + res[i].product_name + " | Price: $" + res[i].price + ".00");
+        }
+        console.log("---------------------------------------------");
+    });
+}
+
+pickItem();
+
+function pickItem() {
+    inquirer
+        .prompt([
+            {
+                name: "item",
+                type: "input",
+                message: "What is the ID# of the item you want to buy?"
+              },
+              {
+                name: "units",
+                type: "input",
+                message: "How many units did you want to buy?"
+              }
+        ])
+        .then(function(answer)  {
+            if (answer.units > stock_quanity)
+            console.log("Insufficient quantity!")
+        })
+}
